188fa412f3784c936b0e5c69c0acf5299b24b9a0
Timothy Taz <timone427@gmail.com>
2025-05-08 18:55:52 -0600

    backend functionality I

--- connect.php
+++ connect.php
@@ -1,16 +1,22 @@
 <?php
+// Database connection details
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "zeta_form";
 
-// Try to connect
+// Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
-  die("❌ Connection failed: " . $conn->connect_error);
-} else {
-  echo "✅ Connected to database successfully.";
-} 
-?> 
+	die("Database connection failed: " . $conn->connect_error);
+}
+echo "✅ Connected successfully to the database!";
+
+
+
+?>
+
+
+
