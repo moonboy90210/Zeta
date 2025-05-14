d218109e8b008ff39db977ef117513ca948898c1
Timothy Taz <timone427@gmail.com>
2025-05-14 14:02:44 -0600

    @media check parameters

--- script.js
+++ script.js
@@ -26,6 +26,21 @@ window.addEventListener("scroll", function () {
   });
 //   -----------------------------------------
 
+// document.addEventListener('scroll', function() {
+//     const background = document.querySelector('.background');
+//     const content = document.querySelector('.content');
+//     const scrollPosition = window.scrollY;
+//     const contentTop = content.offsetTop;
+
+//     if (scrollPosition >= contentTop) {
+//         background.style.position = 'fixed';
+//         background.style.top = '0';
+//     } else {
+//         background.style.position = 'absolute';
+//         background.style.top = '0';
+//     }
+// });
+
 const sections = document.querySelectorAll('.section'); 
 
 const observer = new IntersectionObserver((entries) => {
